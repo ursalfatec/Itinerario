a32adb0ff010b4963fb61271c10217d9bd1d6126
FELIPE DE OLIVEIRA <felipe.oliveira175@fatec.sp.gov.br>
2019-10-27 21:39:08 -0300

    Validação de e-mail e senha do usuário

--- src/br/com/itinerario/model/User.java
+++ src/br/com/itinerario/model/User.java
@@ -1,5 +1,7 @@
 package br.com.itinerario.model;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,7 +11,7 @@ public class User {
 	
 	public User(String email, String senha) throws Exception {
 		this.setEmail(email);
-		this.senha = senha;
+		this.setSenha(senha);		
 	}
 	
 	public void setEmail(String email) throws Exception {
@@ -18,16 +20,40 @@ public class User {
 		}
 	}
 	
-	private boolean isValidEmail(String email) {
+	private boolean isValidEmail(String email) throws Exception {
 		boolean isEmailValid = false;
 		if(email != null && email.length() > 0) {
-			String expression = "^[\\\\w\\\\.-]+@([\\\\w\\\\-]+\\\\.)+[A-Z]{2,4}$";
+			String expression = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			        			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 			Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
 			Matcher matcher = pattern.matcher(email);
 			if(matcher.matches()) {
 				isEmailValid = true;
+			}else {
+				throw new RuntimeException("E-mail inválido!");
 			}
 		}
 		return isEmailValid;
 	}	
+	
+	public void setSenha(String senha) throws Exception {
+		if(senha.length() < 8) {
+			throw new RuntimeException("A senha deve ter no minímo 8 caracteres!");
+		}				
+		this.senha = encryptPassword(senha);
+	}
+	
+	private String encryptPassword(String senha) throws Exception{
+		MessageDigest m = MessageDigest.getInstance("MD5");
+		m.update(senha.getBytes(), 0, senha.length());
+		return new BigInteger(1, m.digest()).toString(16);
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public String getSenha() {
+		return this.senha;
+	}
 }
